952d4d995973b135f2e01f421bb81e32fa2f3c8b
Paulinguis993 <99148906+Pau993@users.noreply.github.com>
2025-04-03 09:30:59 -0500

    Update TaxesCalculator.java

--- src/main/java/edu/eci/arsw/myrestaurant/beans/TaxesCalculator.java
+++ src/main/java/edu/eci/arsw/myrestaurant/beans/TaxesCalculator.java
@@ -3,7 +3,7 @@ package edu.eci.arsw.myrestaurant.beans;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
 
 public interface TaxesCalculator {
-	@Override
+	
 	public float getProductTaxes(RestaurantProduct p);
 	
 }
